16ff1eba888d3f37c4dd7f1f96a2902ce1e774fd
Iced0368 <103271471+Iced0368@users.noreply.github.com>
2022-05-08 23:34:49 +0900

    Update Segement_tree.cpp

--- Data structure/Segement_tree.cpp
+++ Data structure/Segement_tree.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
 #include <algorithm>
 #include <utility>
-#define ull unsigned long long
+#define ll long long
 
 using namespace std;
 
 class seg_tree {
 	int n;
 	int size;
-	int* tree;
+	ll* tree;
 
-	int sum_recursive(int s, int e, int curr_s, int curr_e, int index) {
-		if (s == curr_s && e == curr_e)
+	ll sum_recursive(int s, int e, int cur_s, int cur_e, int index) {
+		if (s == cur_s && e == cur_e)
 			return tree[index];
 
-		int mid = (curr_s + curr_e) / 2;
-
-		if (s < mid) {
-			if (e <= mid)
-				return sum_recursive(s, e, curr_s, mid, 2 * index + 1);
-			else
-				return sum_recursive(s, mid, curr_s, mid, 2 * index + 1) + sum_recursive(mid, e, mid, curr_e, 2 * index + 2);
-		}
-		else
-			return sum_recursive(s, e, mid, curr_e, 2 * index + 2);
+		int mid = (cur_s + cur_e) / 2;
+		ll ret = 0;
+        if (s < mid)
+            ret += sum_recursive(s, min(e, mid), cur_s, mid, 2 * index + 1);
+        if (e > mid)  
+            ret += sum_recursive(max(s, mid), e, mid, cur_e, 2 * index + 2);
+        return ret;
 	}
 public:
-	seg_tree(int _n) {
+	seg_tree(int _n, ll* arr = NULL) {
 		n = _n;
 		int cnt = 0;
 
@@ -39,15 +36,22 @@ public:
 			}
 			size = 2 * (1 << cnt) - 1;
 		}
-		tree = new int[size] {};
+		tree = new ll[size] {};
+
+        if(arr != NULL){
+            for(int i = 0; i < n; i++)
+                tree[i + size/2] = arr[i];
+            for(int i = size/2-1; i >= 0; i--)
+                tree[i] = tree[2*i+1] + tree[2*i+2];
+        }
 	}
 	~seg_tree() {
 		delete[] tree;
 	}
 
-	void update_tree(int index, int value) {
+	void update_tree(int index, ll value) {
 		int i = index + size / 2;
-		int diff = value - tree[i];
+		ll diff = value - tree[i];
 		while (i > 0) {
 			tree[i] += diff;
 			i = (i - 1) / 2;
@@ -55,16 +59,7 @@ public:
 		tree[0] += diff;
 	}
 
-	void add_tree(int index, int value) {
-		int i = index + size / 2;
-		while (i > 0) {
-			tree[i] += value;
-			i = (i - 1) / 2;
-		}
-		tree[0] += value;
-	}
-
-	int sum(int s, int e) {
+	ll sum(int s, int e) {
 		return sum_recursive(s, e, 0, size / 2 + 1, 0);
 	}
 
@@ -75,3 +70,12 @@ public:
 		cout << endl;
 	}
 };
+
+int main(void){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+
+    return 0;
+}
